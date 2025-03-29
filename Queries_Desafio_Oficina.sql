e2e875ba080d7e760bfe931e07d93e6afce5c2eb
Marcos-H-Ramos <marcoshenriqueramos@outlook.com>
2025-03-28 23:58:35 -0300

    doc Alterado README.md e finalizadas queries do desafio

--- Queries_Desafio_Oficina.sql
+++ Queries_Desafio_Oficina.sql
@@ -22,13 +22,36 @@ SELECT c.Nome, SUM(valor) as Entradas
 		ON veiculo_idveiculo = idveiculo AND idCliente = cliente_idCliente1
 	WHERE o.status = 'Aprovado' OR o.status = 'Concluido'
     GROUP BY Nome;
-
--- Recuperar custo de mão de obra, peça e total da ordem
-SELECT n° as Ordem, `Mão de obra`, `Peças`, (`Mão de obra` + `Peças`) Total
------------------------------------------- COntinuar daqui
-
-
+    
+-- Recuperar o custo de mão de obra por equipe
+SELECT 
+    c.`Equipe de mecânicos_idEquipe de mecânicos`,
+    COALESCE(SUM(m.`Valor da Hora`), 0) AS total_valor_hora
+	FROM `Oficina`.`Composição da Equipe` c
+	JOIN `Oficina`.`Mecânico` m 
+    ON m.`idMecânico` IN (
+        c.`Mecânico_idMecânico_Lider`,
+        c.`Mecânico_idMecânico_2`,
+        c.`Mecânico_idMecânico_3`,
+        c.`Mecânico_idMecânico_4`
+    )
+GROUP BY c.`Equipe de mecânicos_idEquipe de mecânicos`;
 
 -- Defina ordenações dos dados com ORDER BY;
 -- Condições de filtros aos grupos – HAVING Statement;
--- Crie junções entre tabelas para fornecer uma perspectiva mais complexa dos dados;
+-- Recuperar valor total de mão de obra por ordem de serviço aprovada ordenado por n° da ordem
+SELECT `n°`,`Status`, `Serviços`, (total_valor_hora * `Horas de Trabalho`) as `Mão de Obra` FROM 
+	(SELECT 
+    c.`Equipe de mecânicos_idEquipe de mecânicos`,
+    COALESCE(SUM(m.`Valor da Hora`), 0) AS total_valor_hora
+	FROM `Oficina`.`Composição da Equipe` c 
+	JOIN `Oficina`.`Mecânico` m 
+    ON m.`idMecânico` IN (
+        c.`Mecânico_idMecânico_Lider`,
+        c.`Mecânico_idMecânico_2`,
+        c.`Mecânico_idMecânico_3`,
+        c.`Mecânico_idMecânico_4`
+    )
+GROUP BY c.`Equipe de mecânicos_idEquipe de mecânicos`) t JOIN `oficina`.`ordem de serviço` USING(`Equipe de mecânicos_idEquipe de mecânicos`)
+HAVING `Status` = 'Aprovado'
+ORDER BY `n°`;
--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # Desafio-modelagem-de-BD-com-modelo-ER-para-oficina
 Resposta ao ultimo desafio do bootcamp DIO/Heineken de modelagem de banco de dados
+
+# Modelando Banco de dados para oficina 
+
+Criado um Banco de Dados Relacional para oficina.
+
+Relacionados Clientes, Veiculos, ordens de serviço, equipes responsaveis, mecanicos, peças e valores de mão de obra.
+
+1. Modelado o banco de dados com MySQL Workbench no Modelo EER;
+	Modelo_EER.mwb e oficina_modeloeer.png
+
+2. A partir do Modelo EER, criado script SQL para aplicar banco de dados;
+	Desafio_2_oficina.sql
+
+3. Usado script SQL com assistente de IA para gerar dados para teste;
+	Persistir_dados_oficina.sql
+
+4. Persistidos dados no BD e testado com algumas consultas SQL com: 
+[x] SELECT Statement
+[x] Filtros com WHERE Statement
+[x] Expressões para gerar atributos derivados
+[x] Ordenações dos dados com ORDER BY
+[x] Filtrar grupos com HAVING Statement
+
+Queries estão no arquivo Queries_Desafio.sql
+
+ Com o BD funcionando foi feito um refinamento no SCript e refletidas as alterações no Modelo  EER refinado.
+
+
